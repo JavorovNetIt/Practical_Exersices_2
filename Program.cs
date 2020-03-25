3b83047e48799996f22df5d5d5bf436c29ac5820
JavorovNetIt <k.javorov.net.it@gmail.com>
2020-03-25 18:23:29 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practical_Exersices_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
